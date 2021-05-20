74752f2ba195a4764afee75c1401e162b08d39c0
Evgeny <teador6676@gmail.com>
2021-05-20 11:50:56 +0400

    refactoring App.js & add state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,37 +5,38 @@ import Footer from '../Footer/Footer';
 import styles from './App.module.css';
 
 class App extends React.Component {
-  render() {
-    const items = [
+  state = {
+    items: [
       {
-        value : 'Написать приложение',
+        value: 'Написать приложение',
         isDone: true
       },
       {
-        value : 'Закончить блок react',
+        value: 'Закончить блок react',
         isDone: false
       },
       {
-        value : 'Выучить английский',
+        value: 'Выучить английский',
         isDone: false
       },
       {
-        value : 'Приступить к следующему блоку',
+        value: 'Приступить к следующему блоку',
         isDone: false
       }
-    ];
+    ]
+  };
 
-    const count =  items.filter(item => item.isDone === false);
+  render() {
+    const count = this.state.items.filter(item => item.isDone === false);
 
-    return(
+    return (
       <div className={styles.wrap}>
         <h1>Список дел:</h1>
         <InputItem />
-        <ItemList items={items}/>
+        <ItemList items={this.state.items} />
         <Footer count={count.length} />
       </div>
     );
   }
 };
-
 export default App;
