8fde242b783e14e5e3b9e233c3f49055cfb7987a
Evgeny <teador6676@gmail.com>
2021-05-20 11:48:21 +0400

    refactoring App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,33 +4,38 @@ import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
 import styles from './App.module.css';
 
-const items = [
-  {
-    value : 'Написать приложение',
-    isDone: true
-  },
-  {
-    value : 'Закончить блок react',
-    isDone: false
-  },
-  {
-    value : 'Выучить английский',
-    isDone: false
-  },
-  {
-    value : 'Приступить к следующему блоку',
-    isDone: false
+class App extends React.Component {
+  render() {
+    const items = [
+      {
+        value : 'Написать приложение',
+        isDone: true
+      },
+      {
+        value : 'Закончить блок react',
+        isDone: false
+      },
+      {
+        value : 'Выучить английский',
+        isDone: false
+      },
+      {
+        value : 'Приступить к следующему блоку',
+        isDone: false
+      }
+    ];
+
+    const count =  items.filter(item => item.isDone === false);
+
+    return(
+      <div className={styles.wrap}>
+        <h1>Список дел:</h1>
+        <InputItem />
+        <ItemList items={items}/>
+        <Footer count={count.length} />
+      </div>
+    );
   }
-];
-
-const count =  items.filter(item => item.isDone === false);
-
-const App = () => (
-  <div className={styles.wrap}>
-    <h1>Список дел:</h1>
-    <InputItem />
-    <ItemList items={items}/>
-    <Footer count={count.length} />
-  </div>);
+};
 
 export default App;
