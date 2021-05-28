72fb4c9d6f3bc6168f0e48b4d633f40cd589e475
Evgeny <teador6676@gmail.com>
2021-05-28 19:13:20 +0400

    add life cycle methods

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -2,15 +2,57 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import styles from './Item.module.css';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import DeleteIcon from '@material-ui/icons/Delete';
+import IconButton from '@material-ui/core/IconButton'
 
-const Item = ({ value, isDone }) => (<span className={
-  classnames({
-    [styles.item]: true,
-    [styles.done]: isDone
-  })
-}>
-  {value}
-</span>);
+class Item extends React.Component {
+  componentDidMount() {
+    console.log('componentDidMount');
+  };
+
+  componentDidUpdate() {
+    console.log('componentDidUpdate');
+  };
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+  };
+
+  render() {
+    const { value, isDone, onClickDone, id, onClickDelete} = this.props;
+
+    return(
+      <div className={styles.item}>
+        <FormControlLabel
+          control={
+            <Checkbox checked={isDone}
+              value='checkedA'
+              color='primary'
+              onClick={() => onClickDone(id)}
+            />
+          }
+          label={
+            <span className={
+              classnames({
+                [styles.item]: true,
+                [styles.done]: isDone
+              })
+            }>
+              { value }
+            </span>
+          }
+        />
+        <div onClick={() => onClickDelete(id)}>
+          <IconButton aria-label='delete'>
+            <DeleteIcon fontSize='small' />
+          </IconButton>
+        </div>
+      </div>
+    );
+  };
+};
 
 Item.defaultProps = {
   value: 'Задача не найдена'
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,30 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Item from '../Item/Item';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import DeleteIcon from '@material-ui/icons/Delete';
-import IconButton from '@material-ui/core/IconButton';
-
 import styles from './ItemList.module.css'
 
-const ItemList = ({ items, onClickDone, onClickDelete }) => (<ul className={styles.item_list}>
-  {items.map(item => <li key={item.id} className={styles.item}>
-    <FormControlLabel
-      control={
-        <Checkbox checked={item.isDone}
-          value="checkedA"
-          color="primary"
-          onClick={() => onClickDone(item.id)}
-        />
-      }
-      label=<Item value={item.value} isDone={item.isDone}/>
+const ItemList = ({ items, onClickDone,  onClickDelete }) => (<ul className={styles.item_list}>
+  {items.map(item => <li key={item.id}>
+    <Item 
+      value={item.value}
+      isDone={item.isDone}
+      onClickDone={onClickDone}
+      id={item.id}
+      onClickDelete={onClickDelete}
     />
-    <div onClick={() => onClickDelete(item.id)}>
-      <IconButton aria-label='delete'>
-        <DeleteIcon fontSize='small' />
-      </IconButton>
-    </div>
   </li>)}
 </ul>);
 
@@ -32,6 +19,6 @@ ItemList.propTypes = {
   items: PropTypes.array.isRequired,
   onClickDone: PropTypes.func.isRequired,
   onClickDelete: PropTypes.func.isRequired
- };
+};
 
 export default ItemList;
